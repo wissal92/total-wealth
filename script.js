af6f2ac89e15ec0a6a8665271463216f0d809a12
wissal <wissal.ouarsafi@gmail.com>
2020-03-26 22:21:39 -0700

    Calculate the total wealth

--- script.js
+++ script.js
@@ -66,8 +66,17 @@ function showMillionaires(){
     updateDOM()
 }
 
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);

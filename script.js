9df2758af01290568238763ecc45d82c5f770f25
wissal <wissal.ouarsafi@gmail.com>
2020-03-26 21:45:07 -0700

    Update the dom with the fetched data

--- script.js
+++ script.js
@@ -27,4 +27,17 @@ async function getRandomUser() {
 
 function addData(obj){
     data.push(obj);
+    updateDOM()
 }
+
+function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  
+    providedData.forEach(item => {
+      const element = document.createElement('div');
+      element.classList.add('person');
+      element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+      main.appendChild(element);
+    });
+  }
